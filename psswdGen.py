0839b8dde337c237823a7cdb1e15411ab5134f2d
n4n0n3t <100702774+GIN963@users.noreply.github.com>
2025-03-23 02:06:25 +0100

    Update psswdGen.py

--- psswdGen.py
+++ psswdGen.py
@@ -7,12 +7,12 @@ parser = argparse.ArgumentParser(description = 'Password generator')
 
 
 parser.add_argument('-l', '--length', type=int, default=12, help="Password length (deflaut: 12)")
-parser.add_argument('-d', '--digits', action='store_true', help="Include digits") #action='store_true' -> si l'utilisateur indique l'option alors le script va prendre en compte les chiffres dans le mdp
+parser.add_argument('-d', '--digits', action='store_true', help="Include digits") 
 parser.add_argument('-s', '--symbols', action='store_true', help="Include symbols")
 parser.add_argument('-u', '--uppercase', action='store_true', help="Include uppercases")
 
 
-args = parser.parse_args() #Python va lire la ligne de commande quand tu exécutes le script et va récupérer ce que l'utilisateur a passé et le stocker dans args.
+args = parser.parse_args() 
 
 
 mdp = string.ascii_lowercase
